4b787986b93bfe039e508bdc53840d87c65cdca2
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-05-03 00:04:56 +0300

    Second Update to Syntax

--- Syntax.cpp
+++ Syntax.cpp
@@ -102,5 +102,57 @@ int main(){
     // 5 & 3   -> 0101 & 0011 = 0001 (1 in decimal)
     // Output: 1
 
-    
+    // ! String Methods
+    // std::string name;
+    // std::cout << "Enter your name: ";
+    // std::getline(std::cin >> std::ws, name);
+    // ? name.append("new substring") = appends the new substring to the end of the string
+    // ? name.at(index) = returns the character at the index
+    // ? name.clear() = clears the string
+    // ? name.compare("string") = returns 0 if the strings are equal, -1 if the first string is less than the second string, 1 if the first string is greater than the second string
+    // ? name.erase(start, length) = erases the substring
+    // ? name.empty() = returns true if the string is empty, false otherwise
+    // ? name.find("substring") = returns the index of the first occurrence of the substring
+    // ? name.find_last_of("substring") = returns the index of the last occurrence of the substring
+    // ? name.insert(start, "new substring") = inserts the new substring at the start index
+    // ? name.replace(start, length, "new substring") = replaces the substring with the new substring
+    // ? name.length() = returns the length of the string
+    // ? name.substr(start, length) = returns a substring of the string
+
+    // ! while loops
+    // while(name.empty()){ 
+    //     std::cout << "Enter your name: ";
+    //     std::getline(std::cin >> std::ws, name);
+    // }
+    // std::cout << "Hello " << name << std::endl;
+
+    // ! do while loops are like while loops, but they run at least once
+    // do{
+    //     std::cout << "Enter your name: ";
+    //     std::getline(std::cin >> std::ws, name);
+    // } while(name.empty());
+    // std::cout << "Hello " << name << std::endl;
+
+    // ! for loops
+    // for(int i = 0; i < 10; i++){
+    //     std::cout << i << std::endl;
+    // }
+
+    // break and continue
+    // break = exits the loop (breaks out of the loop)
+    // continue = skips the current iteration of the loop and goes to the next iteration (skip the current iteration)
+    // for(int i = 0; i < 10; i++){
+    //     if(i == 5){
+    //         break;
+    //     }
+    //     std::cout << i << std::endl;
+    // }
+    // ? Output: 0 1 2 3 4
+    // for(int i = 0; i < 10; i++){
+    //     if(i == 5){
+    //         continue;
+    //     }
+    //     std::cout << i << std::endl;
+    // }
+    // ? Output: 0 1 2 3 4 6 7 8 9
 }
