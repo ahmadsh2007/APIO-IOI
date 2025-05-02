804214f65777bed6d65c15155ce43fb3b154fdcb
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-05-02 22:00:48 +0300

    First Update to Syntax

---
+++ Syntax.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+    // std::string Variable;
+    // std::getline(std::cin >> std::ws, Variable);
+
+    // double x = 3.14159, y = 4, z; 
+    // z = std::max(x, y);
+    // z = std::min(x, y);
+    // z = pow(x, y);
+    // z = sqrt(y);
+    // z = abs(x);
+    // z = ceil(x);
+    // z = floor(x);
+    // z = round(x);
+    // z = sin(x/2);
+
+    // std::cout << "x: " << x << std::endl;
+    // std::cout << "y: " << y << std::endl;
+    // std::cout << "z: " << z << std::endl;
+
+    // std::cout << "Enter Side A: ";
+    // double a, b, c;
+    // std::cin >> a;
+    // std::cout << "Enter Side B: ";
+    // std::cin >> b;
+
+    // c = sqrt(pow(a, 2) + pow(b, 2));
+    // std::cout << "Hypotenuse: " << c << std::endl;
+
+    // ? use switch case instead of if else
+
+    // ? ternary operator ?: = replace to an if else statement
+    // ? condition ? expression1 : expression2;
+    // int grade = 75;
+    // if(grade <= 60){
+    //     std::cout << "You failed the exam." << std::endl;
+    // } else if(grade > 60){
+    //     std::cout << "You passed the exam." << std::endl;
+    // }
+    // ? but instead of using if else statement, we can use a ternary operator
+    // std::cout << (grade <= 60 ? "You failed the exam." : "You passed the exam.") << std::endl;
+    // ? another example of using a ternary operator
+    // int number = 9;
+    // number % 2 == 0 ? std::cout << "Even" : std::cout << "Odd";
+    // ? another example of using a ternary operator with boolean
+    // bool Hungry = true;
+    // Hungry ? std::cout << "You are Hungry" : std::cout << "You are Full";
+
+    // ! Logical Operators
+    // ? Logical AND (&&) = true if both operands are true
+    // Example: 
+    // true && true = true
+    // true && false = false
+    // false && false = false
+
+    // ? Logical OR (||) = true if at least one operand is true
+    // Example: 
+    // true || false = true
+    // false || false = false
+    // true || true = true
+
+    // ? Logical NOT (!) = true if the operand is false and the reverse is true
+    // Example: 
+    // !true = false
+    // !false = true
+
+    // ? Logical XOR (^) = true if exactly one operand is true, but not both
+    // Example:
+    // true ^ false = true (1 ^ 0 = 1)
+    // false ^ true = true (0 ^ 1 = 1)
+    // true ^ true = false (1 ^ 1 = 0)
+    // false ^ false = false (0 ^ 0 = 0)
+
+
+    // ? Logical NOR (!||) = true if both operands are false
+    // Example: 
+    // !true || true = false
+    // !false || false = true
+
+    // ? Logical NAND (!&&) = true if at least one operand is false
+    // Example: 
+    // !(true && true) = false
+    // !(true && false) = true
+    // !(false && false) = true
+
+    // ? Logical XNOR (==) = true if both operands are the same, either true or false
+    // Example: 
+    // true == true = true
+    // false == false = true
+    // true == false = false
+    // false == true = false
+
+    // ? Bitwise OR (|) = compares each bit of two numbers and returns 1 if at least one bit is 1, else returns 0
+    // Example: 
+    // 5 | 3   -> 0101 | 0011 = 0111 (7 in decimal)
+    // Output: 7
+
+    // ? Bitwise AND (&) = compares each bit of two numbers and returns 1 only if both bits are 1, else returns 0
+    // Example: 
+    // 5 & 3   -> 0101 & 0011 = 0001 (1 in decimal)
+    // Output: 1
+
+    
+}
